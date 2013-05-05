a96dc68ddb672f17df0845ff19bf154d0a29765a
Leszek Uchacz <bloodzio@gmail.com>
2013-05-05 22:29:08 +0200

    game.math

--- Warsztat-Android4/src/game/math/Matrix3x3.java
+++ Warsztat-Android4/src/game/math/Matrix3x3.java
@@ -6,11 +6,14 @@
  */
 package game.math;
 
+import game.math.Util;
+
 
 
 public class Matrix3x3
 {
 	private float[] matrix = new float[9];
+	private float[] matrixTmp = new float[9];
 	//   0,1,2,
 	//   3,4,5,
 	//	 6,7,8
@@ -22,12 +25,23 @@ public class Matrix3x3
 
 	public Matrix3x3 mul (Matrix3x3 m) {
 		
+		
 		return this;
 	}
 	public void set(){
 		
 	}
-
+	
+	public Matrix3x3 rotateDegrees (float degrees) {
+		float angle =(float) Util.degreesToRadians * degrees;
+		if (angle == 0) return this;
+		else 
+		
+			
+			
+		return this;
+		
+	}
 
 
 }
---
+++ Warsztat-Android4/src/game/math/Util.java
@@ -0,0 +1,10 @@
+package game.math;
+
+
+public class Util
+{
+
+	static public final float PI = 3.1415927f;
+	static public final float radiansToDegrees = 180f / PI;
+	static public final float degreesToRadians = PI / 180;
+}
